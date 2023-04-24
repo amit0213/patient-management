0125558129f55994b27d29f1e3fcfd577be5fc8a
amit0213 <131682701+amit0213@users.noreply.github.com>
2023-04-24 09:13:21 -0500

    Update PatientController.java
    mappings added

--- src/main/java/capitalone/patientManagement/controller/PatientController.java
+++ src/main/java/capitalone/patientManagement/controller/PatientController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PatientController {
 	
-	
-	 
+	@GetMapping("/showAll")
+	public String testPage(){
+		return "Hello";
+	}
 
 }
